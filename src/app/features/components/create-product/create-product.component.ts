555cd670bd1dc90da505a514bc3c379ce386ae61
Alexandre Junior <alejunior.dev@gmail.com>
2024-05-10 11:23:57 -0300

    feat: add fields into to the form of product creation

--- src/app/features/components/create-product/create-product.component.ts
+++ src/app/features/components/create-product/create-product.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-create-product',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule
+  ],
   templateUrl: './create-product.component.html',
-  styleUrl: './create-product.component.scss'
+  styleUrl: './create-product.component.scss',
 })
 export class CreateProductComponent {
+  form = new FormGroup({
+    title: new FormControl<string>('', {
+      nonNullable: true,
+      validators: Validators.required,
+    }),
+  });
 
+  onSubmit() {
+    this.form.controls.title.value;
+  }
 }
