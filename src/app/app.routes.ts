9499cd6f21c01c11a45431a074e884e1fa954232
Alexandre Junior <alejunior.dev@gmail.com>
2024-05-09 22:09:16 -0300

    feat: add route to create product component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ListComponent } from './features/list/list.component';
+import { CreateProductComponent } from './features/components/create-product/create-product.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: ListComponent,
   },
+  {
+    path: 'create-product',
+    component: CreateProductComponent,
+  }
 ];
