38d5a3900753e89c91424e3b94ef6cc269c4f21f
Alexandre Junior <alejunior.dev@gmail.com>
2024-05-14 15:30:12 -0300

    fix: change name method to edit product

--- src/app/shared/components/form/form.component.ts
+++ src/app/shared/components/form/form.component.ts
@@ -29,7 +29,7 @@ export class FormComponent {
 
   form!: FormGroup;
 
-  @Output() submit = new EventEmitter<Product>();
+  @Output() done = new EventEmitter<Product>();
 
   ngOnInit(): void {
     this.form = new FormGroup({
@@ -42,6 +42,6 @@ export class FormComponent {
 
   onSubmit() {
     const product = this.form.value as Product;
-    this.submit.emit(product);
+    this.done.emit(product);
   }
 }
