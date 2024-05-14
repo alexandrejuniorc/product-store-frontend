e2693e91b63fbde57881392e6fc3ee7968b40be6
Alexandre Junior <alejunior.dev@gmail.com>
2024-05-14 14:45:55 -0300

    feat: create form component

---
+++ src/app/shared/components/form/form.component.ts
@@ -0,0 +1,47 @@
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Product } from '../../@types/product.interface';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+  ],
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+})
+export class FormComponent {
+  product = input<Product | null>(null);
+
+  form!: FormGroup;
+
+  @Output() submit = new EventEmitter<Product>();
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl<string>(this.product()?.title ?? '', {
+        nonNullable: true,
+        validators: Validators.required,
+      }),
+    });
+  }
+
+  onSubmit() {
+    const product = this.form.value as Product;
+    this.submit.emit(product);
+  }
+}
